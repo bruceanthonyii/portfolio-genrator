da869ae887e3164749f57c912e965f55142131bf
Brock <bruceanthonyii@gmail.com>
2021-01-22 11:09:56 -0600

    uploading to show TA the issue

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
+const inquirer = require('inquirer');
+
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username'
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:'
+    }
+  ]);
+};
+promptUser().then(answers => console.log(answers));
 const fs = require('fs');
 const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+
+//console.log(inquirer);
+
+const pageHTML = generatePage(name, github);
+// const profileDataArgs = process.argv.slice(2);
+// const [name, github] = profileDataArgs;
 
 //console.log(profileDataArgs);
 
@@ -25,7 +51,7 @@ const [name, github] = profileDataArgs;
 //console.log(name, github);
 //console.log(generatePage(name, github))
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
+fs.writeFile('./index.html', pageHTML, err => {
   if (err) throw new Error(err);
 
   console.log('Portfolio complete! Check out index.html to see the output!');
